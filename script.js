92717f3925b570c905aa98d2335a7c499f6cc7cc
IronZenith <jackironmalley@gmail.com>
2021-08-28 00:01:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
+/* Tanks */
 dragElement(document.getElementById("pld"));
-dragElement(document.getElementById("war"));
-dragElement(document.getElementById("drk"));
-dragElement(document.getElementById("gnb"));
-dragElement(document.getElementById("blm"));
-dragElement(document.getElementById("smn"));
-dragElement(document.getElementById("rdm"));
-dragElement(document.getElementById("blu"));
-dragElement(document.getElementById("drg"));
-dragElement(document.getElementById("mnk"));
-dragElement(document.getElementById("nin"));
-dragElement(document.getElementById("sam"));
-dragElement(document.getElementById("brd"));
-dragElement(document.getElementById("mch"));
-dragElement(document.getElementById("dnc"));
-dragElement(document.getElementById("whm"));
-dragElement(document.getElementById("sch"));
-dragElement(document.getElementById("ast"));
-
 dragElement(document.getElementById("pld1"));
+dragElement(document.getElementById("war"));
 dragElement(document.getElementById("war1"));
+dragElement(document.getElementById("drk"));
 dragElement(document.getElementById("drk1"));
+dragElement(document.getElementById("gnb"));
 dragElement(document.getElementById("gnb1"));
+/* Casters */
+dragElement(document.getElementById("blm"));
 dragElement(document.getElementById("blm1"));
+dragElement(document.getElementById("smn"));
 dragElement(document.getElementById("smn1"));
+dragElement(document.getElementById("rdm"));
 dragElement(document.getElementById("rdm1"));
+dragElement(document.getElementById("blu"));
 dragElement(document.getElementById("blu1"));
+/* Melee */
+dragElement(document.getElementById("drg"));
 dragElement(document.getElementById("drg1"));
+dragElement(document.getElementById("mnk"));
 dragElement(document.getElementById("mnk1"));
+dragElement(document.getElementById("nin"));
 dragElement(document.getElementById("nin1"));
+dragElement(document.getElementById("sam"));
 dragElement(document.getElementById("sam1"));
+/* Ranged */
+dragElement(document.getElementById("brd"));
 dragElement(document.getElementById("brd1"));
+dragElement(document.getElementById("mch"));
 dragElement(document.getElementById("mch1"));
+dragElement(document.getElementById("dnc"));
 dragElement(document.getElementById("dnc1"));
+/* Healers */
+dragElement(document.getElementById("whm"));
 dragElement(document.getElementById("whm1"));
+dragElement(document.getElementById("sch"));
 dragElement(document.getElementById("sch1"));
+dragElement(document.getElementById("ast"));
 dragElement(document.getElementById("ast1"));
 
+/* Roles */
 dragElement(document.getElementById("tank"));
-dragElement(document.getElementById("healer"));
-dragElement(document.getElementById("melee"));
-dragElement(document.getElementById("ranged"));
-dragElement(document.getElementById("caster"));
 dragElement(document.getElementById("tank1"));
+dragElement(document.getElementById("healer"));
 dragElement(document.getElementById("healer1"));
+dragElement(document.getElementById("melee"));
 dragElement(document.getElementById("melee1"));
+dragElement(document.getElementById("melee2"));
+dragElement(document.getElementById("melee3"));
+dragElement(document.getElementById("ranged"));
 dragElement(document.getElementById("ranged1"));
+dragElement(document.getElementById("ranged2"));
+dragElement(document.getElementById("ranged3"));
+dragElement(document.getElementById("caster"));
 dragElement(document.getElementById("caster1"));
+dragElement(document.getElementById("caster2"));
+dragElement(document.getElementById("caster3"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
