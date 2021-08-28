77aa536c7f0d1451db3f0e2738f4479f52ca3c7b
IronZenith <jackironmalley@gmail.com>
2021-08-27 22:36:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-setTimeout(includeHTML, 2000);
+dragElement(document.getElementById("pld"));
 
 function dragElement(elmnt) {
+  setTimeout(includeHTML, 2000);
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id)) {
     /* if present, the header is where you move the DIV from:*/
@@ -40,5 +41,3 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-
-dragElement(document.getElementById("pld"));
