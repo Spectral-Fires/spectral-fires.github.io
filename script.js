54d55ec320cbe003b7bc6a3f727241019d8f8e45
XIVPlanner <jackironmalley@gmail.com>
2021-08-31 07:28:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,32 @@
     $(window).on("load", function() {
 
         $(function() { $('.container') });
-        $(function() { $('.drag').draggable({
-        }) });
+        $(function() { $('.drag').draggable({scroll: false}) });
         $(function() { $('.resize').draggable()
             .resizable({
             aspectRatio: true,
+            scroll: false
         }); });
         $(function() { $('.fresize').draggable()
             .resizable({
             aspectRatio: false,
             scroll: false
         }); });
-        $(function() { $('.rotate').rotatable({
-            wheelRotate:true, 
-            snap:true, 
-            step: 15, 
-        }) });
+        $(function() { $('.rotate').rotatable({wheelRotate:true, snap:true, step: 15, }) });
+
+        // CREATE MORE DIV, WITH 'ABSOLUTE' POSITIONING.
+        $('#btClickMe').click(function() {
+
+            var dynamic_div = $(document.createElement('div')).css({
+                border: '1px dashed', position: 'absolute', 
+                width: '120', height: '120', padding: '3', margin: '0'
+            });
+
+            element_pos = element_pos + $('.container').width() + 20;
+            
+            // APPEND THE NEWLY CREATED DIV TO "divContainer".
+            $(dynamic_div).appendTo('.container').draggable();
+
+            iCnt = iCnt + 1;
         });
     });
-
-
