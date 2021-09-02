318ffd84d8d3b6e5e3403968f48da3488ef7c062
XIVPlanner <jackironmalley@gmail.com>
2021-09-02 10:56:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-    $(window).on("load", function() {
+// Searches for and calls arena images //
 
-        // CREATE MORE DIV, WITH 'ABSOLUTE' POSITIONING.
-        $('.select').click(function() {
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+alert(urlParams["arena"]);
+
+// Calls new objects and assigns them active functions //
+
+$(window).on("load", function() {
+	// Drag Only //
+	$('.select').click(function() {
 		$(this).clone().appendTo('#catcher').addClass('drag').removeClass('select').removeAttr('id').css({
-          position: 'absolute',
-          left: '30px',
-          top: '30px'
-          })
+	  position: 'absolute',
+		width: '80%',
+		height: '80%'
+	  })
 	$(function() { $('.drag').draggable({containment: 'body'})});
-        });  
-
-        // CREATE MORE DIV, WITH 'ABSOLUTE' POSITIONING.
-        $('.selectr').click(function() {
+	});  
+	// Full Motion / Fixed Aspect Ratio //
+	$('.selectr').click(function() {
 		$(this).clone().appendTo('#catcher').addClass('dragr').removeClass('selectr').removeAttr('id').css({
-          position: 'absolute',
-          left: '5px',
-          top: '5px'
-          })
+	  	position: 'absolute',
+			width: '80%',
+			height: '80%'
+	  })
 	$(function() { $('.dragr').draggable({containment: 'body'}).resizable({aspectRatio: true})});
 	$(function() { $('.rotate').rotatable({wheelRotate:true, snap:true, step: 15, }) });
-        });
-	  
-        // CREATE MORE DIV, WITH 'ABSOLUTE' POSITIONING.
-        $('.selectfr').click(function() {
+	});
+	// Full Motion / Free Ratio //
+	$('.selectfr').click(function() {
 		$(this).clone().appendTo('#catcher').addClass('dragfr').removeClass('selectfr').removeAttr('id').css({
-          position: 'absolute',
-          left: '5px',
-          top: '5px'
-          })
+		  position: 'absolute',
+			width: '80%',
+			height: '80%'
+	  })
 	$(function() { $('.dragfr').draggable({containment: 'body'}).resizable({aspectRatio: false})});
 	$(function() { $('.rotate').rotatable({wheelRotate:true, snap:true, step: 15, }) });
-        });
-	    
-    })
+	});
+
+	})
